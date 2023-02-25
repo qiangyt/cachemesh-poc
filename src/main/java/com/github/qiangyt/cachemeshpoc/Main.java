ef6a3241bee5456d7c7fbc9f31fbf43707de71f5
Yiting Qiang <qiangyt@wxcount.com>
2023-02-25 09:38:49 +0000

    init

---
+++ src/main/java/com/github/qiangyt/cachemeshpoc/Main.java
@@ -0,0 +1,11 @@
+package com.github.qiangyt.cachemeshpoc;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        out.println(args);
+    }
+
+}

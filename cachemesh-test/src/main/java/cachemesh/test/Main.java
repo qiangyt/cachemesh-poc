cf91791249b432551b97a5bb2b5d7db1bf7e8768
Yiting Qiang <qiangyt@wxcount.com>
2023-04-04 01:59:39 +0800

    na

--- cachemesh-test/src/main/java/cachemesh/test/Main.java
+++ cachemesh-test/src/main/java/cachemesh/test/Main.java
@@ -1,10 +1,9 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Copyright © 2023 Yiting Qiang (qiangyt@wxcount.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -18,25 +17,25 @@ package cachemesh.test;
 
 import cachemesh.core.MeshNetwork;
 import cachemesh.core.config.MeshConfig;
-import cachemesh.jgroup.JGroupsListener;
-import cachemesh.jgroup.JGroupsMembers;
+//import cachemesh.jgroup.JGroupsListener;
+//import cachemesh.jgroup.JGroupsMembers;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
-            System.out.println("usage: <say-hello> <url>");
+            System.out.println("usage: <say-hello> <profile>");
             return;
         }
 
         boolean sayHello = "true".equals(args[0]);
         System.out.printf("sayHello=%s\n", sayHello);
 
-        String primaryUrl = args[1];
+        String profile = args[1];
 
         System.out.println("mesh bootstrap: ...");
 
-        var configYamlStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("example.yaml");
+        var configYamlStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(profile + ".yaml");
         var config = MeshConfig.fromYaml(configYamlStream);
         var mesh = MeshNetwork.build(config);
 
@@ -45,20 +44,15 @@ public class Main {
         Thread.sleep(5000);
         System.out.println("mesh bootstrap: done");
 
-        var jgroups = new JGroupsMembers(primaryUrl, mesh.getName(), "jgroups.xml", new JGroupsListener() {
-            @Override
-            public void onNodeJoin(String nodeUrl) throws Exception {
-                System.out.println("join " + nodeUrl);
-                mesh.addRemoteNode(nodeUrl);
-            }
-
-            @Override
-            public void onNodeLeave(String nodeUrl) throws Exception {
-                System.out.println("leave " + nodeUrl);
-                // mesh.remoteNode(nodeUrl);
-            }
-        });
-        jgroups.start();
+        /*
+         * var jgroups = new JGroupsMembers(primaryUrl, mesh.getName(), "jgroups.xml", new JGroupsListener() {
+         *
+         * @Override public void onNodeJoin(String nodeUrl) throws Exception { System.out.println("join " + nodeUrl);
+         * mesh.addRemoteNode(nodeUrl); }
+         *
+         * @Override public void onNodeLeave(String nodeUrl) throws Exception { System.out.println("leave " + nodeUrl);
+         * // mesh.remoteNode(nodeUrl); } }); jgroups.start();
+         */
 
         // String[] nodeUrls = args;
 
@@ -107,7 +101,7 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace(System.err);
         } finally {
-            jgroups.stop();
+            // jgroups.stop();
             mesh.shutdown(0);
         }
     }
